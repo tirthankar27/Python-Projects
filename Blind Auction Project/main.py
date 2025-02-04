fac1551332fbd72cba1c5b07f8cced02e5386cf1
Tirthankar Ghosh <tirthankarghosh4@gmail.com>
2025-02-05 00:37:25 +0530

    Add files via upload

---
+++ Blind Auction Project/main.py
@@ -0,0 +1,16 @@
+from art import logo
+print(logo)
+print("Welcome to the secret auction program.")
+bidders=dict()
+while True:
+    name=input("What is your name?: ")
+    bid=input("What's your bid?: $")
+    bidders[name]=bid
+    option=input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if option=='yes':
+        print("\n"*50)
+    elif option=='no':
+        break
+# TODO-4: Compare bids in dictionary
+highest=max(bidders,key=bidders.get)
+print(f"The winner is {highest} with a bid of {bidders[highest]}")
