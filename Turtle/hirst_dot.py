06c1330240e17ff30d18cc4c3cdce5f8c7b5a102
Tirthankar Ghosh <tirthankarghosh4@gmail.com>
2025-03-14 13:29:15 +0530

    Add files via upload

---
+++ Turtle/hirst_dot.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+
+jimmy=turtle.Turtle()
+my_screen=turtle.Screen()
+turtle.colormode(255)
+jimmy.speed(0)
+
+def change_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    jimmy.pencolor((r, g, b))
+jimmy.hideturtle()
+jimmy.penup()
+jimmy.back(50)
+for i in range(10):
+    for j in range(10):
+        jimmy.pendown()
+        change_color()
+        jimmy.dot(10)
+        jimmy.penup()
+        jimmy.forward(20)
+    jimmy.left(90)
+    jimmy.forward(20)
+    jimmy.right(90)
+    jimmy.back(200)
+my_screen.exitonclick()
