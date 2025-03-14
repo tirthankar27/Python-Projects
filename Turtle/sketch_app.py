2c29de1c582cbe083e8cc6a2063ec9d8ea451bea
Tirthankar Ghosh <tirthankarghosh4@gmail.com>
2025-03-14 20:35:23 +0530

    Add files via upload

--- Turtle/sketch_app.py
+++ Turtle/sketch_app.py
@@ -10,13 +10,19 @@ def move_forward():
 def move_backward():
     jimmy.back(10)
 def turn_left():
-    jimmy.left(4)
+    jimmy.left(2)
 def turn_right():
-    jimmy.right(4)
+    jimmy.right(2)
+def clear_screen():
+    jimmy.clear()
+    jimmy.penup()
+    jimmy.home()
+    jimmy.pendown()
 
 my_screen.listen()
 my_screen.onkey(key='w',fun=move_forward)
 my_screen.onkey(key='s',fun=move_backward)
 my_screen.onkey(key='a',fun=turn_left)
 my_screen.onkey(key='d',fun=turn_right)
+my_screen.onkey(key='c',fun=clear_screen)
 my_screen.exitonclick()
